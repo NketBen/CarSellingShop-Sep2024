77ccf903214071f9e9dca6a925cf5c9ca7166e14
Benson Nketia Bennewa <93406836+NketBen@users.noreply.github.com>
2024-08-31 06:07:49 +0100

    Add files via upload

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,13 +1,15 @@
+//useAuthStatus.js vigila estado de usuario
 import { useEffect, useState } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
+// para seguir estado de usuario actual. Estado puede se loggedIn or not.
 export function useAuthStatus() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [checkingStatus, setCheckingStatus] = useState(true);
 
   useEffect(() => {
     const auth = getAuth();
-    // console.log(auth);
+    
     onAuthStateChanged(auth, (user) => {
       if (user) {
         setLoggedIn(true);
